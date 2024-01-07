76d7f604f4c338c5281071617dcf20cf3a5a0462
Nhan Nguyen <nhannguyen@Nhans-MacBook-Air.local>
2024-01-07 14:54:37 +0700

    Refactor page folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,6 @@ const roboto = Roboto({
   display: "swap",
 });
 
-export const metadata: Metadata = {
-  title: "NextJS Tailwind Campaign Page",
-  description:
-    "Introducing Tailwind Campaign Page, an all-inclusive and visually captivating campaign landing page template built on the foundation of Tailwind CSS and Material Tailwind.",
-};
-
 export default function RootLayout({
   children,
 }: {
